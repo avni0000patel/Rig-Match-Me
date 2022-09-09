a1f34b88e9caccb96cf3d426975d244519a4a8f7
Avni <avni0000patel@gmail.com>
2022-09-08 21:34:10 -0400

    login as homepage

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -3,7 +3,7 @@ const { } = require('../models');
 
 
 
-router.get('/login', (req, res) => {
+router.get('/', (req, res) => {
     // If the user is already logged in, redirect the request to another route
     if (req.session.logged_in) {
         res.redirect('/');
