28da9533f414a1104b8c3909f6ddfe1474b0fba7
Avni <avni0000patel@gmail.com>
2022-09-08 21:13:24 -0400

    heroku push

---
+++ public/js/newRig.js
@@ -0,0 +1,23 @@
+const newFormHandler = async (event) => {
+    event.preventDefault();
+
+    const genre = document.querySelector(`select[name="genre"]`).value.trim();
+
+    const response = await fetch(`/api/rig`, {
+        method: 'POST',
+        body: JSON.stringify({ genre }),
+        headers: {
+            'Content-Type': 'application/json',
+        },
+    });
+
+    if (response.ok) {
+        document.location.replace('/homepage');
+    } else {
+        alert(response.statusText);
+    }
+};
+
+document
+    .querySelector('.new-rig-form')
+    .addEventListener('submit', newFormHandler);
