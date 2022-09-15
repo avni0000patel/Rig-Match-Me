ef80932ad23d08a5c2c973e3da290cc801ab7ec0
Avni <avni0000patel@gmail.com>
2022-09-15 19:20:24 -0400

    heroku

---
+++ controllers/contact_routes.js
@@ -0,0 +1,12 @@
+const router = require("express").Router();
+const { } = require("../models");
+
+router.get("/", async (req, res) => {
+    res.render('contact');
+});
+
+router.post('/send', async (req, res) => {
+    console.log(req.body);
+})
+
+module.exports = router;
--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const path = require('path');
 const express = require('express');
 const session = require('express-session');
 const exphbs = require('express-handlebars');
+const nodemailer = require('nodemailer');
 const routes = require('./controllers');
 const helpers = require('./utils/helpers');
 
@@ -36,7 +37,7 @@ app.engine('handlebars', hbs.engine);
 app.set('view engine', 'handlebars');
 
 app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+app.use(express.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use(routes);
