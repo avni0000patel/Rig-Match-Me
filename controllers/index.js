8e6c5d90dd42b293988fece192ff2b00b0c19ea4
Avni <avni0000patel@gmail.com>
2022-09-16 18:59:48 -0400

    add contact us page

--- controllers/index.js
+++ controllers/index.js
@@ -3,11 +3,11 @@ const router = require("express").Router();
 const apiRoutes = require("./api");
 const homeRoutes = require("./home_routes");
 const newRigRoutes = require("./new_rig_routes");
-const contactRoutes = require("./contact_routes");
+// const contactRoutes = require("./contact_routes");
 
 router.use("/", homeRoutes);
 router.use("/api", apiRoutes);
 router.use("/new_rig", newRigRoutes);
-router.use("/contact", contactRoutes);
+// router.use("/contact", contactRoutes);
 
 module.exports = router;
