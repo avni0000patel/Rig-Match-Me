ada6e1473bb0011edbf75d164f0235ba1f6f474c
alvinkp <102767990+alvinkp@users.noreply.github.com>
2022-09-15 23:18:53 -0400

    Updated JS
    *ACCESSORIES_ROUTES*
    
    - Added accessories routes
    
    *INDEX.JS*
    
    - Added accessoriesRoutes
    
    *NEW_RIG.JS*
    
    - Working on addFirstAccessory()
    
    - Added returnAccessory()

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -4,11 +4,13 @@ const genreRoutes = require("./genre_routes");
 const instrumentRoutes = require("./instrument_routes");
 const rigRoutes = require("./rig_routes");
 const instrumentGenreRoutes = require("./instrument_genres_routes");
+const accessoriesRoutes = require("./accessories_routes");
 
 router.use("/users", userRoutes);
 router.use("/genres", genreRoutes);
 router.use("/instruments", instrumentRoutes);
 router.use("/rigs", rigRoutes);
 router.use("/instgenre", instrumentGenreRoutes);
+router.use("/accessories", accessoriesRoutes);
 
 module.exports = router;
